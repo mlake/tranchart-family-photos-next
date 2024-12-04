258d7d4cfafa77744557683625164af555dc48ee
Michael Lake <mlake@alleywayapps.com>
2024-12-04 13:07:26 +0100

    adding descritpion

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,19 +8,19 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <meta
             name="description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="Old glass negatives found in the family attic"
           />
-          <meta property="og:site_name" content="nextjsconf-pics.vercel.app" />
+          <meta property="og:site_name" content="tranchart.alleywayapps.com" />
           <meta
             property="og:description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="Old glass negatives found in the family attic"
           />
-          <meta property="og:title" content="Next.js Conf 2022 Pictures" />
+          <meta property="og:title" content="Plaques famile Tranchart" />
           <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="Next.js Conf 2022 Pictures" />
+          <meta name="twitter:title" content="Plaques famile Tranchart" />
           <meta
             name="twitter:description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="Old glass negatives found in the family attic"
           />
         </Head>
         <body className="bg-black antialiased">
